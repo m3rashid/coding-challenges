606512d4dbf86be2182fd5a0f146a9f63e99cbe6
m3rashid <m3rashid.hussain@gmail.com>
2024-02-10 12:37:50 +0530

    stack

---
+++ internal/stack/main.go
@@ -0,0 +1,53 @@
+package stack
+
+import "fmt"
+
+type Stack[T interface{}] struct {
+	items        []T
+	stackPointer int
+}
+
+func New[T any]() *Stack[T] {
+	return &Stack[T]{
+		items:        []T{},
+		stackPointer: -1,
+	}
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.stackPointer++
+	s.items = append(s.items, item)
+}
+
+func (s *Stack[T]) Pop() (T, error) {
+	if s.stackPointer < 0 {
+		var res T
+		return res, fmt.Errorf("stack is empty")
+	}
+
+	item := s.items[s.stackPointer]
+	s.items = s.items[:s.stackPointer]
+	s.stackPointer--
+	return item, nil
+}
+
+func (s *Stack[T]) Top() (T, error) {
+	if s.stackPointer < 0 {
+		var res T
+		return res, fmt.Errorf("stack is empty")
+	}
+	return s.items[s.stackPointer], nil
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return s.stackPointer < 0
+}
+
+func (s *Stack[T]) Size() int {
+	return s.stackPointer + 1
+}
+
+func (s *Stack[T]) Clear() {
+	s.items = []T{}
+	s.stackPointer = -1
+}
