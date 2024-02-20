0883b3fb02d9fc76f5b6d05b177f02234db0efc6
m3rashid <m3rashid.hussain@gmail.com>
2024-02-20 19:31:51 +0530

    add priority queue

---
+++ internal/priorityQueue/main.go
@@ -0,0 +1,34 @@
+package priorityqueue
+
+type Node struct {
+	Char  rune
+	Freq  int
+	Left  *Node
+	Right *Node
+}
+
+type PriorityQueue []*Node
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Freq < pq[j].Freq
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(*Node))
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
